c7e812d2e6c3265d0b3e27b7cc3263ce0da1698b
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-07-07 11:36:42 -0500

    First commit

---
+++ _config/adminSidebar.js
@@ -0,0 +1,3 @@
+const pages = config('pages') // Get Pages from config
+export default [
+]
---
+++ _config/apiRoutes.js
@@ -0,0 +1,10 @@
+const moduleName = 'setup';
+const moduleVersion = 'v1';
+const urlBase = `/${moduleName}/${moduleVersion}`
+
+
+export default {
+  urlBase : urlBase,
+  version: moduleVersion,
+  companies: `${urlBase}/companies`,
+}
