1a5a75861f22375e6b04ee91759749148df4f3dc
Juan Escalante <jcec007@gmail.com>
2023-01-10 15:00:41 -0500

    +Update: activate areas permission adminPages

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -105,7 +105,7 @@ export default {
     }
   },
   areas: {
-    //permission: 'setup.areas.manage',
+    permission: 'setup.areas.manage',
     activated: true,
     authenticated: true,
     path: '/setup/areas',
