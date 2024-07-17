bdb7bcbcb2ef31cdf68cf37e33a8661a7adbb1f9
WilmerAce <wilmerc@imaginacolombia.com>
2024-07-17 11:24:06 -0500

    +update: change url agione for url dynamic

---
+++ _crud/baseCrud.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'RampCrud',
+  components: {},
+  data() {
+    return {
+      crudId: this.$uid(),
+    }
+  },
+  provide() {
+    return {}
+  },
+  computed: {
+    crudData() {
+      return {
+        crudId: this.crudId,
+        read: {},
+        update: false,
+        delete: true,
+      }
+    },
+    //Crud info
+    crudInfo() {
+      return this.$store.state.qcrudComponent.component[this.crudId] || {}
+    }
+  },
+  methods: {}
+}
+</script>
+<style lang="stylus">
+</style>
